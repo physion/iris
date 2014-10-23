205eac8a90e38900a768f5797002978ecd13ef4d
Barry Wark <barrywark@gmail.com>
2014-10-23 15:39:49 -0400

    Update to modern clutch

--- src/iris/couch.clj
+++ src/iris/couch.clj
@@ -40,38 +40,9 @@
 (defn get-document
   [db-name doc-id &{:keys [rev]}]
   (if rev
-    (cl/get-document (database db-name) doc-id {:rev rev} #{:rev})
+    (cl/get-document (database db-name) doc-id {:rev rev})
     (cl/get-document (database db-name) doc-id)))
 
 (defn get-underworld-document
   [doc-id]
   (cl/get-document @db doc-id))
-
-;(defn watched-state
-;  [database-name]
-;  (ensure-db)
-;  (-> (cl/get-document @db database-name)
-;    (cl/dissoc-meta)))
-;
-;(defn set-watched-state!
-;  [database-name last-seq]
-;  (ensure-db)
-;  (-> (if-let [doc (cl/get-document @db database-name)]
-;        (cl/put-document @db (assoc doc :last-seq last-seq))
-;        (cl/put-document @db {:_id database-name :last-seq last-seq}))
-;    (cl/dissoc-meta)))
-;
-;(defn changes-since
-;  "Returns all database changes since the given sequence (a string) for the database db"
-;  [db-name since]
-;  (let [url (database db-name)]
-;    (if (nil? since)
-;      (cl/changes url :include_docs true)
-;      (cl/changes url :since since :include_docs true))))
-;
-;(defn webhooks
-;  "Gets all webhooks for the given database for updated documents with the given type"
-;  [database                                                 ; :- s/Str
-;   type]                                                    ; :- document "type" value
-;  (ensure-webhooks)
-;  (cl/get-view @db osiris-design-doc :webhooks {:include_docs true} {:key [database type]}))
