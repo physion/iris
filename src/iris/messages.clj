4b0f9e90603c274bb07032baa5a82a7194cce357
Barry Wark <barrywark@gmail.com>
2014-10-24 17:04:51 -0400

    WIP http-kit calls

--- src/iris/messages.clj
+++ src/iris/messages.clj
@@ -1,13 +1,15 @@
 (ns iris.messages
   (:refer-clojure :exclude [send])
   (:require [org.httpkit.client :as http]
-            [schema.core :as s]
-            [iris.schema :refer [NewReceipt NewMessage]]
-            [iris.couch :as couch]))
+            [iris.schema :refer [NewReceipt NewMessage Receipt]]
+            [iris.couch :as couch]
+            [ring.util.http-response :refer [throw! get-type]]
+            [org.httpkit.client :as http]
+            [clojure.data.json :as json]))
 
 
-(s/defn send :- Receipt
-  [msg :- NewMessage]
+(defn send
+  [msg]
   (let [{doc-id  :doc_id
          doc-rev :doc_rev
          db-name :db
@@ -16,8 +18,13 @@
         doc (couch/get-document db-name doc-id :rev doc-rev)
         hook (couch/get-underworld-document hook-id)]
 
-    {:type    "receipt"
-     :db      db-name
-     :doc_id  doc-id
-     :doc_rev doc-rev
-     :hook_id hook-id}))
+    (let [response (http/post (:url doc) {:body (json/write-str doc)})]
+
+      (if (.success? (get-type (:status @response)))
+
+        {:type    "receipt"
+         :db      db-name
+         :doc_id  doc-id
+         :doc_rev doc-rev
+         :hook_id hook-id}
+        (throw! @response)))))
