e3f720d0dc168228391631376b48b0f3e629d14e
Barry Wark <barrywark@gmail.com>
2015-02-04 21:36:12 -0500

    Trying any schema

--- src/iris/handler.clj
+++ src/iris/handler.clj
@@ -11,26 +11,26 @@
 
 ;; --- Routes --- ;;
 (defapi app
-  (swaggered "iris"
-    (HEAD* "/" []
-      (ok ""))
-    (GET* "/" []
-      (ok "Iris!"))
+        (swaggered "iris"
+                   (HEAD* "/" []
+                          (ok ""))
+                   (GET* "/" []
+                         (ok "Iris!"))
 
-    (POST* "/messages" []
-      :body [msg NewMessage]
-      :summary "Processes an update from Osiris"
-      :header-params [x-aws-sqsd-msgid :- s/Str
-                      x-aws-sqsd-queue :- s/Str
-                      x-aws-sqsd-first-received-at :- s/Str
-                      x-aws-sqsd-receive-count :- s/Str]
+                   (POST* "/messages" []
+                          :body [msg s/Any]
+                          :summary "Processes an update from Osiris"
+                          :header-params [x-aws-sqsd-msgid :- s/Str
+                                          x-aws-sqsd-queue :- s/Str
+                                          x-aws-sqsd-first-received-at :- s/Str
+                                          x-aws-sqsd-receive-count :- s/Str]
 
-      ; (-> msg
-      ;(assoc :sqs-msgid x-aws-sqsd-msgid)
-      ;(assoc :sqs-queue x-aws-sqsd-queue)
-      ;(assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
-      ;(assoc :sqs-receive-count (Integer/parseInt x-aws-sqsd-receive-count)))
-      (let [msg-info msg]
 
-        (logging/info "[Iris] Update received" msg-info)
-        (ok (messages/send msg-info))))))
+                          (let [msg-info (-> msg
+                                             (assoc :sqs-msgid x-aws-sqsd-msgid)
+                                             (assoc :sqs-queue x-aws-sqsd-queue)
+                                             (assoc :sqs-first-received-at x-aws-sqsd-first-received-at)
+                                             (assoc :sqs-receive-count (Integer/parseInt x-aws-sqsd-receive-count)))]
+
+                            (logging/info "[Iris] Update received" msg-info)
+                            (ok (messages/send msg-info))))))
